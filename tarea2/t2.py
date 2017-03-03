d170214752696c1754b796636b6828cacad173af
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-03-03 18:30:36 +0100

    Inicio de sesión closes #7

--- tarea2/t2.py
+++ tarea2/t2.py
@@ -1,14 +1,35 @@
-from flask import Flask, Response, render_template
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/')
 def home():
-    return render_template('text.html')
+    if 'username' in session:
+        return render_template('text.html')
+    return render_template('sign-in.html')
 
 @app.route('/imagen')
 def imagen():
-    return render_template('image.html')
+    if 'username' in session:
+        return render_template('image.html')
+    return render_template('sign-in.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['inputUsername']
+        print("hola")
+        return redirect(url_for('home'))
+    print("hola2")
+    return render_template('sign-in.html')
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session.pop('username', None)
+        return redirect(url_for('home'))
+    return render_template('sign-in.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
