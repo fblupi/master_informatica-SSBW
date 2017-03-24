eb175354dbb098ec45262b6372c66e00784b562d
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-03-24 17:50:33 +0100

    Usando ids closes #29

--- tarea3/populate.py
+++ tarea3/populate.py
@@ -20,7 +20,7 @@ class likes(EmbeddedDocument):
 
 class restaurants(Document):
     name             = StringField(required=True, max_length=80)
-    restaurant_id    = IntField()
+    restaurant_id    = IntField(min_value=1, unique=True)
     cuisine          = StringField()
     borough          = StringField()
     address          = EmbeddedDocumentField(addr)
@@ -42,7 +42,7 @@ def downloadAndInsert(bar, city, cuisine, borough):
     coordXML = [float(locationXML[0].xpath('//lat/text()')[0]), float(locationXML[0].xpath('//lng/text()')[0])]
 
     a = addr(building=buildingXML, street=streetXML, city=cityXML, zipcode=zipcodeXML, coord=coordXML)
-    r = restaurants(name=bar, cuisine=cuisine, borough=borough, address=a)
+    r = restaurants(name=bar, restaurant_id=restaurants.objects.count() + 1, cuisine=cuisine, borough=borough, address=a)
     r.save()
 
 # Rellenar BD
